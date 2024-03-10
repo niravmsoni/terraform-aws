b60324cbc019007916ed793600069f9250c44176
Nirav Soni <soninirav910@gmail.com>
2024-03-10 17:28:34 +0530

    Update

--- BuildingBlocks/3_Resource/Code/main.tf
+++ BuildingBlocks/3_Resource/Code/main.tf
@@ -115,3 +115,38 @@ resource "aws_nat_gateway" "nat_gateway" {
     Name = "demo_nat_gateway"
   }
 }
+
+# Create S3 bucket
+resource "aws_s3_bucket" "my-new-S3-bucket" {
+bucket = "my-new-tf-test-bucket-nirav-random-string"
+tags = {
+Name = "My S3 Bucket"
+Purpose = "Intro to Resource Blocks Lab"
+}
+}
+
+# Create Bucket Ownership resource
+resource "aws_s3_bucket_ownership_controls" "my_new_bucket_acl" {
+bucket = aws_s3_bucket.my-new-S3-bucket.id
+rule {
+object_ownership = "BucketOwnerPreferred"
+}
+}
+
+# Create Security group
+resource "aws_security_group" "my-new-security-group" {
+name = "web_server_inbound"
+description = "Allow inbound traffic on tcp/443"
+vpc_id = aws_vpc.vpc.id
+ingress {
+description = "Allow 443 from the Internet"
+from_port = 443
+to_port = 443
+protocol = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
+}
+tags = {
+Name = "web_server_inbound"
+Purpose = "Intro to Resource Blocks Lab"
+}
+}
