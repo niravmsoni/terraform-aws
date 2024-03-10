8dff682ada4f748fc438d8958a8cb7427409acd2
Nirav Soni <soninirav910@gmail.com>
2024-03-10 17:51:10 +0530

    Updates

---
+++ BuildingBlocks/4_Variables/README.md
@@ -0,0 +1,31 @@
+# Variables in Terraform
+- While working with Terraform templates we should focus on reusability and DRY development (don’t
+repeat yourself)
+- Input variables allow aspects of a module or configuration to be customized without altering the module’s own source code. This allows modules to be shared between different configurations
+- Variables would most often be declared in a separate file (variables.tf) although its not mandatory
+- Variables are declared in a variable block - one block for each
+variable. The variable block contains the variable name, most importantly, and then often includes
+additional information such as the type, a description, a default value, and other options.
+
+- Syntax
+
+```hcl
+variable "<VAR_NAME>" {
+    type = <DATA_TYPE>
+    description = <DESCRIPTION>
+    default = <EXPRESSION>
+    sensitive = <BOOLEAN>
+    validation = <RULES>
+}
+
+variable "aws_region" {
+type = string
+description = "region used to deploy workloads"
+default = "us-east-1"
+validation {
+condition = can(regex("^us-", var.aws_region))
+error_message = "The aws_region value must be a valid region in the
+USA, starting with \"us-\"."
+}
+}
+```
---
+++ BuildingBlocks/4_Variables/code/main.tf
@@ -0,0 +1,158 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+#Retrieve the list of AZs in the current AWS region
+data "aws_availability_zones" "available" {}
+data "aws_region" "current" {}
+
+#Define the VPC
+resource "aws_vpc" "vpc" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name        = var.vpc_name
+    Environment = "demo_environment"
+    Terraform   = "true"
+  }
+}
+
+#Deploy the private subnets
+resource "aws_subnet" "private_subnets" {
+  for_each          = var.private_subnets
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, each.value)
+  availability_zone = tolist(data.aws_availability_zones.available.names)[each.value]
+
+  tags = {
+    Name      = each.key
+    Terraform = "true"
+  }
+}
+
+#Deploy the public subnets
+resource "aws_subnet" "public_subnets" {
+  for_each                = var.public_subnets
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = cidrsubnet(var.vpc_cidr, 8, each.value + 100)
+  availability_zone       = tolist(data.aws_availability_zones.available.names)[each.value]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name      = each.key
+    Terraform = "true"
+  }
+}
+
+#Create route tables for public and private subnets
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    gateway_id     = aws_internet_gateway.internet_gateway.id
+    #nat_gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+  tags = {
+    Name      = "demo_public_rtb"
+    Terraform = "true"
+  }
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    # gateway_id     = aws_internet_gateway.internet_gateway.id
+    nat_gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+  tags = {
+    Name      = "demo_private_rtb"
+    Terraform = "true"
+  }
+}
+
+#Create route table associations
+resource "aws_route_table_association" "public" {
+  depends_on     = [aws_subnet.public_subnets]
+  route_table_id = aws_route_table.public_route_table.id
+  for_each       = aws_subnet.public_subnets
+  subnet_id      = each.value.id
+}
+
+resource "aws_route_table_association" "private" {
+  depends_on     = [aws_subnet.private_subnets]
+  route_table_id = aws_route_table.private_route_table.id
+  for_each       = aws_subnet.private_subnets
+  subnet_id      = each.value.id
+}
+
+#Create Internet Gateway
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "demo_igw"
+  }
+}
+
+#Create EIP for NAT Gateway
+resource "aws_eip" "nat_gateway_eip" {
+  domain     = "vpc"
+  depends_on = [aws_internet_gateway.internet_gateway]
+  tags = {
+    Name = "demo_igw_eip"
+  }
+}
+
+#Create NAT Gateway
+resource "aws_nat_gateway" "nat_gateway" {
+  depends_on    = [aws_subnet.public_subnets]
+  allocation_id = aws_eip.nat_gateway_eip.id
+  subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id
+  tags = {
+    Name = "demo_nat_gateway"
+  }
+}
+
+# Create S3 bucket
+resource "aws_s3_bucket" "my-new-S3-bucket" {
+# Updating bucket name to be unique by adding random provider
+bucket = "my-new-tf-test-bucket-nirav-${random_id.randomness.hex}"
+tags = {
+Name = "My S3 Bucket"
+Purpose = "Intro to Resource Blocks Lab"
+}
+}
+
+# Create Bucket Ownership resource
+resource "aws_s3_bucket_ownership_controls" "my_new_bucket_acl" {
+bucket = aws_s3_bucket.my-new-S3-bucket.id
+rule {
+object_ownership = "BucketOwnerPreferred"
+}
+}
+
+# Create Security group
+resource "aws_security_group" "my-new-security-group" {
+name = "web_server_inbound"
+description = "Allow inbound traffic on tcp/443"
+vpc_id = aws_vpc.vpc.id
+ingress {
+description = "Allow 443 from the Internet"
+from_port = 443
+to_port = 443
+protocol = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
+}
+tags = {
+Name = "web_server_inbound"
+Purpose = "Intro to Resource Blocks Lab"
+}
+}
+
+# Creating random string for using in S3 bucket name
+resource "random_id" "randomness" {
+byte_length = 16
+}
